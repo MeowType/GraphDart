d9a0489712a894b01c665377353d47de10aa28c5
2A5F <Aditrc@outlook.com>
2019-08-15 19:00:19 +0800

    undirected

--- lib/src/graphs/undirected.dart
+++ lib/src/graphs/undirected.dart
@@ -3,47 +3,71 @@ part of meowtype.graph;
 /// Undirected graph, Can create an edge between 2 nodes
 abstract class UndirectedGraph extends GraphItems {
   factory UndirectedGraph() = FullGraph;
+
   /// create an link between 2 nodes
-  void link(a, b);
+  void link(a, b, {List tags});
+
   /// Determine if there is an link between 2 nodes, **No matter what direction**
-  bool hasLink(a, b);
+  bool hasLink(a, b, {List anyTags, List allTags});
+
   /// Remove the link between 2 nodes
-  bool unLink(a, b);
+  bool unLink(a, b, {List anyTags, List allTags});
+
   /// Get all the links of this node
-  Iterable links(val);
+  Iterable links(val, {List anyTags, List allTags});
 }
 
 /// Mixing of implementations of [UndirectedGraph]
 mixin UndirectedGraphMixin on GraphItemsMixin implements UndirectedGraph {
-  void link(a, b) {
-    final _a = _map_add_or_get(a, _newNode);
-    final _b = _map_add_or_get(b, _newNode);
-    _a.setFrom(_b);
-    _b.setFrom(_a);
-    _a.setTo(_b);
-    _b.setTo(_a);
+  void link(a, b, {List tags = const []}) {
+    final t = _Tuple2(a, b).map((v) => _map_add_or_get(v, _newNode));
+    _mutualT(t, (f, t) => f.setFrom(t));
+    _mutualT(t, (f, t) => f.setTo(t));
+    _mutualT(t, (f, t) => f.setTag(t, tags));
   }
 
-  bool hasLink(a, b) {
-    final _a = _map_add_or_get(a, _newNode);
-    final _b = _map_add_or_get(b, _newNode);
-    return _a.hasTo(_b) || _b.hasTo(_a);
+  bool hasLink(a, b, {List anyTags = const [], List allTags = const []}) {
+    return _Tuple2(a, b)
+        .map((v) => _map_add_or_get(v, _newNode))
+        .mutual((f, t) => _check_hasTo_and_all_any_tags(f, t,
+            anyTags: anyTags, allTags: allTags))
+        .toDo(_or);
   }
 
-  bool unLink(a, b) {
-    final _a = _map_add_or_get(a, _newNode);
-    final _b = _map_add_or_get(b, _newNode);
-    final x = _a.unsetTo(_b);
-    final y = _a.unsetFrom(_b);
-    final z = _b.unsetTo(_a);
-    final w = _b.unsetFrom(_a);
-    return x || y || z || w;
+  bool unLink(a, b, {List anyTags = const [], List allTags = const []}) {
+    return _Tuple2(a, b)
+        .map((v) => _map_add_or_get(v, _newNode))
+        .where((t) => t
+            .mutual((f, t) => _check_hasTo_and_all_any_tags(f, t,
+                anyTags: anyTags, allTags: allTags))
+            .toDo(_or))
+        .some((t) => t
+            .fork()
+            .mapFn<bool Function(_Node, _Node), _Tuple2<bool>>(
+                (t) => (fn) => t.mutual(fn))
+            .allDo((f, t) => f.unsetTo(t), (f, t) => f.unsetFrom(t))
+            .map((t) => t.toDo(_or))
+            .toDo(_or))
+        .defaultVal(false)
+        .val;
   }
 
-  Iterable links(val) {
+  Iterable links(val, {List anyTags = const [], List allTags = const []}) {
     final _v = _map_add_or_get(val, _newNode);
-    final from = _v.from.map((n) => _node_to_val[n]);
-    final to = _v.to.keys.map((n) => _node_to_val[n]);
-    return _concat(from, to).toSet();
+    if (anyTags.length == 0 && allTags.length == 0) {
+      final from = _v.from.map((n) => _node_to_val[n]);
+      final to = _v.to.keys.map((n) => _node_to_val[n]);
+      return _concat(from, to).toSet();
+    } else {
+      final from = _v.from
+          .where((n) =>
+              _check_all_any_tags(n, _v, anyTags: anyTags, allTags: allTags))
+          .map((n) => _node_to_val[n]);
+      final to = _v.to.keys
+          .where((n) =>
+              _check_all_any_tags(_v, n, anyTags: anyTags, allTags: allTags))
+          .map((n) => _node_to_val[n]);
+      return _concat(from, to).toSet();
+    }
   }
 }
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,64 @@
 part of meowtype.graph;
 
+class _Tuple2<T> {
+  final T a;
+  final T b;
+  _Tuple2(this.a, this.b);
+
+  /// return `fn(a, b)`
+  R toDo<R>(R fn(T a, T b)) => fn(a, b);
+
+  /// `x = toDo(a)` and `y = toDo(b)` then return `Tuple2(x, y)`
+  _Tuple2<R> map<R>(R toDo(T v)) => _Tuple2(toDo(a), toDo(b));
+
+  /// `x = toDo(a, b)` and `y = toDo(b, a)` then return `Tuple2(x, y)`
+  _Tuple2<R> mutual<R>(R toDo(T f, T t)) => _Tuple2(toDo(a, b), toDo(b, a));
+
+  /// return `Tuple2(Tuple2(a, b), Tuple2(a, b))`
+  _Tuple2<_Tuple2<T>> fork() => _Tuple2(_Tuple2(a, b), _Tuple2(a, b));
+
+  /// return `Tuple2(Tuple2(a, a), Tuple2(b, b))`
+  _Tuple2<_Tuple2<T>> pack() => _Tuple2(_Tuple2(a, a), _Tuple2(b, b));
+
+  /// `x = doa(a)` and `y = dob(b)` then return `Tuple2(x, y)`
+  _Tuple2<R> allDo<R>(R doa(T a), R dob(T b)) => _Tuple2(doa(a), dob(b));
+
+  /// `x = toDo(a)` and `y = toDo(b)` then return `Tuple2(x, y)`
+  _FnTuple2<F, R> mapFn<F extends Function, R>(R Function(F) toDo(T v)) =>
+      _FnTuple2(toDo(a), toDo(b));
+
+  Maybe<_Tuple2<T>> where(bool fn(_Tuple2<T> t)) =>
+      fn(this) ? Some(_Tuple2(a, b)) : const None();
+}
+
+class _FnTuple2<F extends Function, R> {
+  final F a;
+  final F b;
+  _FnTuple2(this.a, this.b);
+
+  _Tuple2<R> allDo(F doa, F dob) => _Tuple2(a(doa), b(dob));
+}
+
+/// `x = toDo(a, b)` and `y = toDo(b, a)` then return `Tuple2(x, y)`
+_Tuple2<R> _mutual<T, R>(T a, T b, R toDo(T f, T t)) {
+  final r1 = toDo(a, b);
+  final r2 = toDo(b, a);
+  return _Tuple2(r1, r2);
+}
+
+/// `x = toDo(a, b)` and `y = toDo(b, a)` then return `Tuple2(x, y)`
+_Tuple2<R> _mutualT<T, R>(_Tuple2<T> t, R toDo(T f, T t)) {
+  final r1 = toDo(t.a, t.b);
+  final r2 = toDo(t.b, t.a);
+  return _Tuple2(r1, r2);
+}
+
+/// `a -> b -> a || b`
+bool _or(bool a, bool b) => a || b;
+
+/// `a -> b -> a && b`
+bool _and(bool a, bool b) => a && b;
+
 V _add_or_get<K, V>(Map<K, V> m, K key, V Function() def) {
   if (m.containsKey(key)) return m[key];
   final val = def();
@@ -17,6 +76,7 @@ Iterable<V> _concat<V>(Iterable<V> a, Iterable<V> b) sync* {
   yield* b;
 }
 
+/// return **`false`** when allTags and anyTags are empty
 bool _check_all_any_tags(
   _Node from,
   _Node to, {
@@ -29,6 +89,7 @@ bool _check_all_any_tags(
   return result;
 }
 
+/// return **`true`** when allTags and anyTags are empty
 bool _check_hasTo_and_all_any_tags(
   _Node from,
   _Node to, {
@@ -42,10 +103,11 @@ bool _check_hasTo_and_all_any_tags(
   return false;
 }
 
-
+/// return **`false`** when allTags and anyTags are empty
 bool _check_all_any_val_tags(
   _Node from,
-  _Node to, key, {
+  _Node to,
+  key, {
   List anyTags = const [],
   List allTags = const [],
 }) {
@@ -55,15 +117,18 @@ bool _check_all_any_val_tags(
   return result;
 }
 
+/// return **`true`** when allTags and anyTags are empty
 bool _check_hasToVal_and_all_any_val_tags(
   _Node from,
-  _Node to, key, {
+  _Node to,
+  key, {
   List anyTags = const [],
   List allTags = const [],
 }) {
   if (from.hasToV(to, key)) {
     if (allTags.length == 0 && anyTags.length == 0) return true;
-    return _check_all_any_val_tags(from, to, key, allTags: allTags, anyTags: anyTags);
+    return _check_all_any_val_tags(from, to, key,
+        allTags: allTags, anyTags: anyTags);
   }
   return false;
 }
